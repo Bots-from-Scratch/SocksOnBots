8f36b4340d614807cc96d2d5938ec9902efb15dd
D-Jinnz Nathan <djinnz.nathan@gmail.com>
2023-01-01 18:04:59 +0100

    added menu options and logo; cleaned files

--- src/PreloadScene.js
+++ src/PreloadScene.js
@@ -1,3 +1,4 @@
+import logo from "./assets/menu/logo.png";
 import {Scene} from "phaser";
 
 class PreloadScene extends Scene {
@@ -5,12 +6,17 @@ constructor() {
     super('preload');
 }
     preload() {
-
+        this.load.image('logo', logo);
     }
 
     create() {
-        const playButton = this.add.text(100, 100, 'Start Game')
-        this.input.on('pointerdown', () => this.scene.start('game'))
+        this.add.image(400, 50, 'logo');
+        const playButton = this.add.text(100, 250, 'START GAME');
+        const options = this.add.text(100, 350, 'OPTIONS');
+        const credits = this.add.text(100, 450, 'CREDITS');
+        playButton.setInteractive();
+        playButton.on('pointerdown', () => this.scene.start('game'));
+        // this.input.on('pointerdown', () => this.scene.start('game'));
     }
 
 }
