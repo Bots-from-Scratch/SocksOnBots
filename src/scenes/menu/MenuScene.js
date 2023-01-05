1f3ca833039c80b3be450367769b96115029c29f
D-Jinnz Nathan <djinnz.nathan@gmail.com>
2023-01-05 19:32:12 +0100

    Menüpunkte mit eigenen Scenes Fixes #3

--- src/scenes/menu/MenuScene.js
+++ src/scenes/menu/MenuScene.js
@@ -1,23 +1,36 @@
 import logo from "../../assets/menu/logo.png";
-import {Scene} from "phaser";
+import button from "../../assets/menu/button.png";
+import block from "../../assets/menu/block.png";
+import { Scene } from "phaser";
 
 class MenuScene extends Scene {
-constructor() {
-    super('menu');
-}
+    constructor() {
+        super('menu');
+    }
     preload() {
         this.load.image('logo', logo);
+        this.load.image('button', button);
+        this.load.image('block', block);
     }
 
     create() {
-        this.add.image(400, 50, 'logo');
-        const playButton = this.add.text(100, 250, 'START GAME');
-        const options = this.add.text(100, 350, 'OPTIONS');
-        const credits = this.add.text(100, 450, 'CREDITS');
+        this.add.image(400, 100, 'logo').setScale(0.15, 0.15);
 
-        playButton.setInteractive().on('pointerdown', () => this.scene.start('game'));
-        options.setInteractive().on('pointerdown', () => this.scene.start('options'));
-        credits.setInteractive().on('pointerdown', () => this.scene.start('credits'))
+        const playButton = this.add.text(100, 250, 'START GAME').setFontSize(24).setFontStyle('bold');
+        const options = this.add.text(100, 350, 'OPTIONS').setFontSize(20);
+        const credits = this.add.text(100, 450, 'CREDITS').setFontSize(20);
+
+        console.log(playButton.getLeftCenter());
+
+        const menuPointer = this.add.rectangle(60, 250 + (playButton.displayHeight / 2), 10, 10).setFillStyle(16777215);
+
+        playButton.setInteractive({ useHandCursor: true })
+            .on('pointerdown', () => this.scene.start('game'));
+        options.setInteractive({ useHandCursor: true })
+            .on('pointerdown', () => this.scene.start('options'));
+            // .on('pointerover', () => menuPointer.setY(this.displayHeight));
+        credits.setInteractive({ useHandCursor: true })
+            .on('pointerdown', () => this.scene.start('credits'));
     }
 
 }
--- src/scenes/menu/OptionScene.js
+++ src/scenes/menu/OptionScene.js
@@ -10,7 +10,7 @@ class OptionScene extends Scene {
     }
 
     create() {
-        this.add.image(400, 50, 'logo');
+        this.add.image(400, 100, 'logo').setScale(0.15, 0.15);
         const sound = this.add.text(100, 250, 'SOUND');
         const back = this.add.text(100, 450, '<- BACK');
         back.setInteractive().on('pointerdown', () => this.scene.start('menu'));
