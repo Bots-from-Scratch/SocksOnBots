e1a158e84fdc02b11d1827c41abb4ef87521257a
D-Jinnz Nathan <djinnz.nathan@gmail.com>
2023-02-01 17:55:40 +0100

    [WIP] Cursor

--- src/scenes/menu/MenuScene.js
+++ src/scenes/menu/MenuScene.js
@@ -7,33 +7,60 @@ class MenuScene extends Scene {
     constructor() {
         super('menu');
     }
+
+    init() {
+        const cursors = this.input.keyboard.createCursorKeys();
+    }
+
     preload() {
         this.load.svg('logo', logo);
         this.load.image('button', button);
         this.load.image('block', block);
     }
 
-    create() {
-        this.add.image(475, 100, 'logo').setScale(1, 1);
-
-        const playButton = this.add.text(100, 250, 'START GAME').setFontSize(24).setFontStyle('bold');
-        const options = this.add.text(100, 350, 'OPTIONS').setFontSize(20);
-        const credits = this.add.text(100, 450, 'CREDITS').setFontSize(20);
-
-        console.log(playButton.getLeftCenter());
-
-        playButton.setInteractive({ useHandCursor: true })
-        .on('pointerdown', () => this.scene.start('game'));
-        
-        options.setInteractive({ useHandCursor: true })
-        .on('pointerdown', () => this.scene.start('options'));
-        // .on('pointerover', () => menuPointer.setY(this.displayHeight));
-        credits.setInteractive({ useHandCursor: true })
-        .on('pointerdown', () => this.scene.start('credits'));
-        
-        const menuPointer = this.add.rectangle(60, 250 + (playButton.displayHeight / 2), 10, 10).setFillStyle(16777215);
+    getNextButton(state) {
+        if(state === 0) return menuItems.length - 1;
+        if(state === menuItems.length -1) return 0;
+        return menuState + state;
     }
 
+    create() {
+        this.add.image(475, 100, 'logo').setScale(1, 1); // Logo
+        let menuState = 0;
+
+        const startGameButton = this.add.text(100, 250, 'START GAME').setFontSize(24).setFontStyle('bold');
+        const optionsButton = this.add.text(100, 350, 'OPTIONS').setFontSize(20);
+        const creditsButton = this.add.text(100, 450, 'CREDITS').setFontSize(20);
+
+        const menuItems = [startGameButton, optionsButton, creditsButton];
+
+        console.log(startGameButton.getLeftCenter(), startGameButton.y);
+
+        startGameButton.setInteractive({ useHandCursor: true })
+            .on('pointerdown', () => this.scene.start('game'));
+
+        optionsButton.setInteractive({ useHandCursor: true })
+            .on('pointerdown', () => this.scene.start('options'));
+        // .on('pointerover', () => menuPointer.setY(this.displayHeight));
+        creditsButton.setInteractive({ useHandCursor: true })
+            .on('pointerdown', () => this.scene.start('credits'));
+
+        let menuPointer = this.add.rectangle(60, menuItems[menuState].y + (menuItems[menuState].displayHeight / 2), 10, 10).setFillStyle(16777215);
+    }
+
+    update() {
+        
+        const upJustPressed = Phaser.Input.Keyboard.JustDown(cursors.up);
+        const downJustPressed = Phaser.Input.Keyboard.JustDown(cursors.down);
+
+        if(upJustPressed) {
+            console.log(selectNextButton(-1));
+        }
+        if(downJustPressed) {
+            console.log(selectNextButton(1));
+        }
+
+    }
 }
 
 export default MenuScene
