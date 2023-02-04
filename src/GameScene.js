0552f7ea5206c00612865912bb6f3eaf9011a153
Stefan <mohrst84031@th-nuernberg.de>
2023-02-04 17:15:11 +0100

    add spritesheet with more moves

--- src/GameScene.js
+++ src/GameScene.js
@@ -1,10 +1,9 @@
 import {Scene} from 'phaser';
-import sky from "./assets/sky.png";
 import platform from "./assets/platform.png";
 import star from "./assets/socke.png";
 import bomb from "./assets/bomb.png";
-import dude from "./assets/dude.png";
-import tilemap from "./assets/sock-on-bots64.json";
+import bot_sock from "./assets/Spritesheet.png"
+import level_1 from "./assets/SocksOnBots_lvl_1.json";
 import tileset from "./assets/CosmicLilac_Tiles_64x64-cd3.png";
 import {code, playGame} from "./index";
 
@@ -22,6 +21,8 @@ class GameScene extends Scene {
     }
 
     init() {
+
+        this.level = 1;
 
         this.score = 0;
 
@@ -56,8 +57,8 @@ class GameScene extends Scene {
         this.load.image('ground', platform);
         this.load.image('star', star);
         this.load.image('bomb', bomb);
-        this.load.spritesheet('dude', dude, {frameWidth: 32, frameHeight: 48});
-        this.load.tilemapTiledJSON('map', tilemap);
+        this.load.spritesheet('bot', bot_sock, {frameWidth: 64, frameHeight: 64});
+        this.load.tilemapTiledJSON('map', level_1);
     }
 
     create() {
@@ -65,12 +66,23 @@ class GameScene extends Scene {
         const map = this.make.tilemap({key: 'map'});
 
 
-        const tileset = map.addTilesetImage('CosmicLilac_Tiles_64x64', 'tileset');
+        const tileset = map.addTilesetImage('CosmicLilac_Tiles_64x64-cd3', 'tileset');
         const backgroundLayer = map.createLayer('background', tileset, 0, 0);
-        const groundLayer = map.createLayer('ground', tileset, 0, 0);
+        const groundLayer = map.createLayer('floor', tileset, 0, 0);
         const wallLayer = map.createLayer('walls', tileset, 0, 0);
 
         wallLayer.setCollisionByProperty({collision: true});
+        // wallLayer.setCollisionFromCollisionGroup(true, true);
+        // wallLayer.renderDebug(this.add.graphics());
+
+        // for (let i = 0 ; i<105; i++) console.log(tileset.getTileCollisionGroup(i));
+
+        const collisionRect = new Phaser.Geom.Rectangle();
+
+        // let gidMapEntries = wallLayer.tileset[0].getTileData(0);
+
+        // for (const el of gidMapEntries) console.log(el[1]);
+
         map.setBaseTileSize(64, 64);
 
 
@@ -147,10 +159,10 @@ class GameScene extends Scene {
         this.platforms = this.physics.add.staticGroup();
 
         // this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-        this.platforms.create(512, 128, 'ground').setScale(0.66, 8.1).setAlpha(0).refreshBody();
-
-
-        this.platforms.create(864, 288, 'ground').setScale(0.5,6).setAlpha(0).refreshBody();
+        // this.platforms.create(512, 128, 'ground').setScale(0.66, 8.1).setAlpha(0).refreshBody();
+        //
+        //
+        // this.platforms.create(864, 288, 'ground').setScale(0.5,6).setAlpha(0).refreshBody();
         // this.platforms.create(50, 250, 'ground');
         // this.platforms.create(750, 220, 'ground');
 
@@ -160,7 +172,11 @@ class GameScene extends Scene {
     }
 
     createPlayer() {
-        this.player = this.physics.add.sprite(150, 150, 'dude').setScale(1.8);
+        this.player = this.physics.add.sprite(150, 150, 'bot').setScale(1);
+        if (this.level = 1) {
+            this.player.setX(480).setY(600);
+        }
+        ;
         // this.player.body.bounce.set(1);
         this.player.body.setMaxSpeed(160);
         this.player.setCircle(14, 2, 24);
@@ -193,24 +209,111 @@ class GameScene extends Scene {
 
         this.anims.create({
             key: 'left',
-            frames: this.anims.generateFrameNumbers('dude', {start: 0, end: 3}),
-            frameRate: 10,
+            frames: this.anims.generateFrameNumbers('bot', {start: 24, end: 29}),
+            frameRate: 5,
             repeat: -1
         });
 
         this.anims.create({
-            key: 'turn',
-            frames: [{key: 'dude', frame: 4}],
-            frameRate: 20
+            key: 'turnToFront',
+            frames: this.anims.generateFrameNumbers('bot', {frames: [0]}),
+            frameRate: 5
+        });
+
+        this.anims.create({
+            key: 'turnToSide',
+            frames: this.anims.generateFrameNumbers('bot', {start: 0, end: 2}),
+            frameRate: 5,
+            repeat: 0
+        });
+
+        this.anims.create({
+            key: 'leftToRight',
+            frames: this.anims.generateFrameNumbers('bot', {frames: [6, 7, 0, 1, 2]}),
+            frameRate: 5,
+            repeat: 0
         });
 
         this.anims.create({
             key: 'right',
-            frames: this.anims.generateFrameNumbers('dude', {start: 5, end: 8}),
-            frameRate: 10,
+            frames: this.anims.generateFrameNumbers('bot', {start: 24, end: 29}),
+            frameRate: 5,
             repeat: -1
         });
+
+        this.anims.create({
+            key: 'up',
+            frames: this.anims.generateFrameNumbers('bot', {start: 32, end: 37}),
+            frameRate: 5,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'down',
+            frames: this.anims.generateFrameNumbers('bot', {start: 8, end: 13}),
+            frameRate: 5,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'leftToUp',
+            frames: this.anims.generateFrameNumbers('bot', {start: 6, end: 4}),
+            frameRate: 5,
+            repeat: 0
+        });
+
+        this.anims.create({
+            key: 'rightToUp',
+            frames: this.anims.generateFrameNumbers('bot', {start: 2, end: 4}),
+            frameRate: 5,
+            repeat: 0
+        });
+
+        this.anims.create({
+            key: 'downToUp',
+            frames: this.anims.generateFrameNumbers('bot', {start: 0, end: 4}),
+            frameRate: 5,
+            repeat: 0
+        });
+
+        this.anims.create({
+            key: 'upToDown',
+            frames: this.anims.generateFrameNumbers('bot', {start: 4, end: 0}),
+            frameRate: 5,
+            repeat: 0
+        });
+
+        this.anims.create({
+            key: 'upToRight',
+            frames: this.anims.generateFrameNumbers('bot', {start: 4, end: 2}),
+            frameRate: 5,
+            repeat: 0
+        });
+
+        this.anims.create({
+            key: 'leftToDown',
+            frames: this.anims.generateFrameNumbers('bot', {frames: [6, 7, 0]}),
+            frameRate: 5,
+            repeat: 0
+        });
+
+        this.anims.create({
+            key: 'rightToDown',
+            frames: this.anims.generateFrameNumbers('bot', {start: 2, end: 0}),
+            frameRate: 5,
+            repeat: 0
+        });
+
+
+
+
+
+
+
+
+
     }
+
 
     createCursor() {
         this.cursors = this.input.keyboard.createCursorKeys();
@@ -360,7 +463,6 @@ class GameScene extends Scene {
                     .strokeCircle(this.player.x, this.player.y, distClosest).strokeRect(this.player.x - distCheb, this.player.y - distCheb, 2 * distCheb, 2 * distCheb);
 
 
-
                 this.gfx.clear()
                     .lineStyle(2, 0xff3300)
                     .lineBetween(this.objectCollidedWith.x, this.objectCollidedWith.y, this.player.x, this.player.y);
@@ -390,27 +492,79 @@ class GameScene extends Scene {
 
         }
 
+
+        if (this.rotation === this.ROTATION_LEFT) {
+            this.player.flipX = true;
+            this.player.anims.playAfterRepeat('left');
+        } else if (this.rotation === this.ROTATION_RIGHT) {
+            this.player.flipX = false;
+            this.player.anims.playAfterRepeat('right');
+        } else if (this.rotation === this.ROTATION_UP) {
+            this.player.flipX = false;
+            this.player.anims.playAfterRepeat('up');
+        } else if (this.rotation === this.ROTATION_DOWN) {
+            this.player.flipX = false;
+            this.player.anims.playAfterRepeat('down');
+        }
+
+
         if (this.cursors.left.isDown || this.direction === 'LEFT') {
+
+            if (this.rotation !== this.ROTATION_LEFT) {
+                this.player.anims.play('turnToSide', true);
+            }
             this.rotation = this.ROTATION_LEFT;
             // this.player.setVelocityX(-160);
             this.player.setVelocityY(0);
-            this.player.anims.play('left', true);
+
+
         } else if (this.cursors.right.isDown || this.direction === 'RIGHT') {
+            if (this. rotation !== this.ROTATION_RIGHT) {
+                if (this.rotation === this.ROTATION_LEFT) {
+                    this.player.anims.play('leftToRight');
+                } else if (this.rotation === this.ROTATION_UP) {
+                    this.player.anims.play('upToRight');
+                } else if (this.rotation === this.ROTATION_DOWN) {
+                    this.player.anims.play('turnToSide');
+                }
+            }
             this.rotation = this.ROTATION_RIGHT;
             // this.player.setVelocityX(160);
             this.player.setVelocityY(0);
-            this.player.anims.play('right', true);
         } else {
             // player.setVelocityX(0);
-
-            this.player.anims.play('turn');
+            // this.player.flipX = false;
+            // this.player.anims.play('turnToFront');
         }
 
         if (this.cursors.up.isDown || this.direction === 'UP') {
+
+            if (this. rotation !== this.ROTATION_UP) {
+                if (this.rotation === this.ROTATION_LEFT) {
+                    this.player.anims.play('leftToUp');
+                } else if (this.rotation === this.ROTATION_RIGHT) {
+                    this.player.anims.play('rightToUp');
+                } else if (this.rotation === this.ROTATION_DOWN) {
+                    this.player.anims.play('downToUp');
+                }
+            }
+
             this.rotation = this.ROTATION_UP;
             this.player.setVelocityX(0);
             // this.player.setVelocityY(-160);
+
         } else if (this.cursors.down.isDown || this.direction === 'DOWN') {
+
+            if (this. rotation !== this.ROTATION_DOWN) {
+                if (this.rotation === this.ROTATION_LEFT) {
+                    this.player.anims.play('leftToDown');
+                } else if (this.rotation === this.ROTATION_RIGHT) {
+                    this.player.anims.play('rightToDown');
+                } else if (this.rotation === this.ROTATION_UP) {
+                    this.player.anims.play('upToDown');
+                }
+            }
+
             this.rotation = this.ROTATION_DOWN;
             this.player.setVelocityX(0);
             // this.player.setVelocityY(160);
